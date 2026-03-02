8f2406d0c4e05d155cef26b2c51d213ea6c5b371
Dawid Ząbek <dawidzabek3@wp.pl>
2026-03-02 21:45:30 +0100

    Initial commit - depot app

---
+++ app/models/product.rb
@@ -0,0 +1,17 @@
+class Product < ApplicationRecord
+  has_one_attached :image
+  after_commit -> { broadcast_refresh_later_to "products" }
+  validates :title, :description, :image, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :title, uniqueness: true
+  validate :acceptable_image
+
+  def acceptable_image
+    return unless image.attached?
+
+    acceptable_types = [ "image/jpeg", "image/png", "image/gif" ]
+    unless acceptable_types.include?(image.content_type)
+      errors.add(:image, "must be a JPEG, PNG, or GIF")
+    end
+  end
+end
