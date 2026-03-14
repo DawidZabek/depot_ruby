8fd43ec8da18657f3c18166c571355ed0f6ddb23
Dawid Ząbek <dawidzabek3@wp.pl>
2026-03-14 01:24:42 +0100

    Add cart functionality

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,14 @@
 class Product < ApplicationRecord
   has_one_attached :image
+  has_many :line_items
   after_commit -> { broadcast_refresh_later_to "products" }
+
   validates :title, :description, :image, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
+
   validate :acceptable_image
+  before_destroy :ensure_not_referenced_by_any_line_item
 
   def acceptable_image
     return unless image.attached?
@@ -12,6 +16,16 @@ class Product < ApplicationRecord
     acceptable_types = [ "image/jpeg", "image/png", "image/gif" ]
     unless acceptable_types.include?(image.content_type)
       errors.add(:image, "must be a JPEG, PNG, or GIF")
+    end
+  end
+
+  private
+
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, "Line Items present")
+      throw :abort
     end
   end
 end
